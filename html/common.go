243e8260e73b8ed58fcfd130b3787379816036b7
solethus <solethu@ssongca.com>
2024-10-22 17:14:07 +0200

    update imports

--- html/common.go
+++ html/common.go
@@ -1,4 +1,4 @@
-// Package HTML holds all the common HTML components and utilities.
+// Package html holds all the common HTML components and utilities.
 package html
 
 import (
@@ -9,9 +9,9 @@ import (
 	"strings"
 	"sync"
 
-	. "maragu.dev/gomponents"
-	. "maragu.dev/gomponents/components"
-	. "maragu.dev/gomponents/html"
+	g "maragu.dev/gomponents"
+	c "maragu.dev/gomponents/components"
+	h "maragu.dev/gomponents/html"
 )
 
 var hashOnce sync.Once
@@ -24,7 +24,7 @@ type PageProps struct {
 }
 
 // page layout with header, footer, and container to restrict width and set base padding.
-func page(props PageProps, children ...Node) Node {
+func page(props PageProps, children ...g.Node) g.Node {
 	// Hash the paths for easy cache busting on changes
 	hashOnce.Do(func() {
 		appCSSPath = getHashedPath("public/styles/app.css")
@@ -32,21 +32,21 @@ func page(props PageProps, children ...Node) Node {
 		appJSPath = getHashedPath("public/scripts/app.js")
 	})
 
-	return HTML5(HTML5Props{
+	return c.HTML5(c.HTML5Props{
 		Title:       props.Title,
 		Description: props.Description,
 		Language:    "en",
-		Head: []Node{
-			Link(Rel("stylesheet"), Href(appCSSPath)),
-			Script(Src(htmxJSPath), Defer()),
-			Script(Src(appJSPath), Defer()),
+		Head: []g.Node{
+			h.Link(h.Rel("stylesheet"), h.Href(appCSSPath)),
+			h.Script(h.Src(htmxJSPath), h.Defer()),
+			h.Script(h.Src(appJSPath), h.Defer()),
 		},
-		Body: []Node{Class("bg-indigo-600 text-gray-900"),
-			Div(Class("min-h-screen flex flex-col justify-between bg-white"),
+		Body: []g.Node{h.Class("bg-indigo-600 text-gray-900"),
+			h.Div(h.Class("min-h-screen flex flex-col justify-between bg-white"),
 				header(),
-				Div(Class("grow"),
+				h.Div(h.Class("grow"),
 					container(true, true,
-						Group(children),
+						g.Group(children),
 					),
 				),
 				footer(),
@@ -56,13 +56,13 @@ func page(props PageProps, children ...Node) Node {
 }
 
 // header bar with logo and navigation.
-func header() Node {
-	return Div(Class("bg-indigo-600 text-white shadow"),
+func header() g.Node {
+	return h.Div(h.Class("bg-indigo-600 text-white shadow"),
 		container(true, false,
-			Div(Class("h-16 flex items-center justify-between"),
-				A(Href("/"), Class("inline-flex items-center text-xl font-semibold"),
-					Img(Src("/images/logo.png"), Alt("Logo"), Class("h-12 w-auto bg-white rounded-full mr-4")),
-					Text("Home"),
+			h.Div(h.Class("h-16 flex items-center justify-between"),
+				h.A(h.Href("/"), h.Class("inline-flex items-center text-xl font-semibold"),
+					h.Img(h.Src("/images/logo.png"), h.Alt("Logo"), h.Class("h-12 w-auto bg-white rounded-full mr-4")),
+					g.Text("Home"),
 				),
 			),
 		),
@@ -70,23 +70,23 @@ func header() Node {
 }
 
 // container restricts the width and sets padding.
-func container(padX, padY bool, children ...Node) Node {
-	return Div(
-		Classes{
+func container(padX, padY bool, children ...g.Node) g.Node {
+	return h.Div(
+		c.Classes{
 			"max-w-7xl mx-auto":     true,
 			"px-4 md:px-8 lg:px-16": padX,
 			"py-4 md:py-8":          padY,
 		},
-		Group(children),
+		g.Group(children),
 	)
 }
 
 // footer with a link to the gomponents website.
-func footer() Node {
-	return Div(Class("bg-indigo-600 text-white"),
+func footer() g.Node {
+	return h.Div(h.Class("bg-indigo-600 text-white"),
 		container(true, false,
-			Div(Class("h-16 flex items-center justify-center"),
-				A(Href("https://www.gomponents.com"), Text("www.gomponents.com")),
+			h.Div(h.Class("h-16 flex items-center justify-center"),
+				h.A(h.Href("https://www.gomponents.com"), g.Text("www.gomponents.com")),
 			),
 		),
 	)
